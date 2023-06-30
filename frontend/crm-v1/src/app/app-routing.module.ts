bcd086852eae8a8cd89d076685c80522077b45ef
unknown <ramanathamith@gmail.com>
2023-06-30 21:09:27 +0530

    Deleted unwanted files.

--- frontend/crm-v1/src/app/app-routing.module.ts
+++ frontend/crm-v1/src/app/app-routing.module.ts
@@ -2,25 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
-import { TicketCardComponent } from './admin-dashboard/tickets-view/ticket-card/ticket-card.component';
+// import { TicketCardComponent } from './admin-dashboard/tickets-view/ticket-card/ticket-card.component';
 import { AgentsAnalyticsComponent } from './admin-dashboard/agents-view/agents-analytics/agents-analytics.component';
 import { TicketsViewComponent } from './admin-dashboard/tickets-view/tickets-view.component';
 import { AgentsViewComponent } from './admin-dashboard/agents-view/agents-view.component';
 import { CustomersViewComponent } from './admin-dashboard/customers-view/customers-view.component';
-import { TicketAnalyticsComponent } from './admin-dashboard/tickets-view/ticket-analytics/ticket-analytics.component';
-import { TicketViewPanelComponent } from './admin-dashboard/tickets-view/ticket-view-panel/ticket-view-panel.component';
+// import { TicketAnalyticsComponent } from './admin-dashboard/tickets-view/ticket-analytics/ticket-analytics.component';
+
 
 const routes: Routes = [
   {path:'admin', component: AdminDashboardComponent,
     children: [
       {path: '', component: TicketsViewComponent},
-      {path: 'tickets', component: TicketsViewComponent , children: [
-        {path: '', component: TicketViewPanelComponent},
-        {path: 'tickets-panel', component: TicketViewPanelComponent},
-        {path: 'tickets-analytics', component: TicketAnalyticsComponent},
-        {path: 'tickets-categories', component: TicketAnalyticsComponent}
-        
-      ]},
+      {path: 'tickets', component: TicketsViewComponent},
       {path: 'agents', component: AgentsViewComponent},
       {path: 'customers', component: CustomersViewComponent},
       
--- frontend/crm-v1/src/app/app.module.ts
+++ frontend/crm-v1/src/app/app.module.ts
@@ -3,14 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-// import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
-// import { TicketCardComponent } from './admin-dashboard/ticket-card/ticket-card.component';
-import { TicketAnalyticsComponent } from './admin-dashboard/tickets-view/ticket-analytics/ticket-analytics.component';
-// import { AgentsViewComponent } from './admin-dashboard/agents-view/agents-view.component';
-// import { AgentsAnalyticsComponent } from './admin-dashboard/agents-analytics/agents-analytics.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
-// import { AdminDashboardModule } from './admin-dashboard/admin-dashboard.module';
 import { NgModule } from '@angular/core';
 import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
 import { AgentsAnalyticsComponent } from './admin-dashboard/agents-view/agents-analytics/agents-analytics.component';
@@ -19,7 +12,6 @@ import { TicketCardComponent } from './admin-dashboard/tickets-view/ticket-card/
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { TicketsViewComponent } from './admin-dashboard/tickets-view/tickets-view.component';
 import { CustomersViewComponent } from './admin-dashboard/customers-view/customers-view.component';
-import { TicketViewPanelComponent } from './admin-dashboard/tickets-view/ticket-view-panel/ticket-view-panel.component';
 import { AgentDetailsTableComponent } from './admin-dashboard/agents-view/agent-details-table/agent-details-table.component';
 
 @NgModule({
@@ -33,8 +25,6 @@ import { AgentDetailsTableComponent } from './admin-dashboard/agents-view/agent-
     AdminDashboardComponent,
     TicketsViewComponent,
     CustomersViewComponent,
-    TicketViewPanelComponent,
-    TicketAnalyticsComponent,
     AgentDetailsTableComponent
   ],
   imports: [
